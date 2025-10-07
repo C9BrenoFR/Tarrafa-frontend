a93dfdd0b55e5f51ab4f62bba455b25313337ee3
C9BrenoFR <brenufr@gmail.com>
2025-10-07 13:08:54 -0300

    Padronização para receber uso de api

--- src/components/Paginas/Curso/Curso.tsx
+++ src/components/Paginas/Curso/Curso.tsx
@@ -7,17 +7,13 @@ import Ranking_Melhor_Desempenho from './Ranking_Melhor_Desempenho/Ranking_Melho
 import Ranking_Mais_Dificuldade from './Ranking_Mais_Dificuldade/Ranking_Mais_Dificuldade';
 import DadosGerais from './DadosGerais/DadosGerais';
 import { getCursos } from '../../../utils/mocks';
-import { DisciplinaType } from '@/types/disciplina';
+import { Curso as CursoType } from '@/types/curso';
 
 type CursoProps = {
-  cursos: DisciplinaType[];
-  cursoSelecionado: number | null;
+  curso: CursoType
 };
 
-export default function Curso({ cursoSelecionado }: CursoProps) {
-  const cursos = getCursos();
-
-  const curso = cursos.find(c => c.id === cursoSelecionado);
+export default function Curso({ curso }: CursoProps) {
 
   return (
     <div className="flex-1 flex justify-center items-center pl-[240px]">
@@ -25,42 +21,30 @@ export default function Curso({ cursoSelecionado }: CursoProps) {
         <div className="flex flex-row justify-between items-start w-full">
           <div className="flex flex-col items-start">
             <h1 className="text-xl font-poppins font-semibold text-left">Disciplina</h1>
-            {curso ? (
-              <p style={{ color: '#374DAA' }} className="text-left text-xl font-semibold">
-                {curso.nome}
-              </p>
-            ) : (
-              <p className="text-left">Nenhuma disciplina foi selecionada ainda.</p>
-            )}
+            <p style={{ color: '#374DAA' }} className="text-left text-xl font-semibold">
+              {curso.nome}
+            </p>
           </div>
           <div className="flex flex-col items-end">
-            {curso ? (
-              <>
-                <p className="text-sm text-right">{curso.data}</p>
-                <p className="text-xl text-right font-poppins font-semibold">{curso.shortname}</p>
-              </>
-            ) : (
-              <p></p>
-            )}
+            <>
+              <p className="text-sm text-right">{curso.data}</p>
+              <p className="text-xl text-right font-poppins font-semibold">{curso.shortname}</p>
+            </>
           </div>
         </div>
         <div>
-          {curso ? (
-            <div className="center-wrapper flex flex-col justify-between">
-              <DadosGerais cursoSelecionado={cursoSelecionado} />
-              <Indicators cursoSelecionado={cursoSelecionado} />
-              <div className="flex flex-row space-x-5">
-                <Grafico cursoSelecionado={cursoSelecionado} />
-                <NumAbso cursoSelecionado={cursoSelecionado} />
-              </div>
-              <div className="flex flex-row space-x-5">
-                <Ranking_Melhor_Desempenho cursoSelecionado={cursoSelecionado} />
-                <Ranking_Mais_Dificuldade cursoSelecionado={cursoSelecionado} />
-              </div>
+          <div className="center-wrapper flex flex-col justify-between">
+            <DadosGerais cursoSelecionado={curso.id} />
+            <Indicators cursoSelecionado={curso.id} />
+            <div className="flex flex-row space-x-5">
+              <Grafico cursoSelecionado={curso.id} />
+              <NumAbso cursoSelecionado={curso.id} />
             </div>
-          ) : (
-            <p></p>
-          )}
+            <div className="flex flex-row space-x-5">
+              <Ranking_Melhor_Desempenho cursoSelecionado={curso.id} />
+              <Ranking_Mais_Dificuldade cursoSelecionado={curso.id} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
