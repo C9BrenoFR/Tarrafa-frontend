f759db196b4a4c4bdfa1210c5318896a1d699352
C9BrenoFR <brenufr@gmail.com>
2025-10-29 09:16:28 -0300

    feat: adiciona configurações da api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,16 @@
 import { Curso } from "@/types/curso";
 import axios from "axios";
 
+const base_url = process.env.NEXT_PUBLIC_API_BASE_URL || ''
+const username = process.env.NEXT_PUBLIC_API_USERNAME || ''
+const password = process.env.NEXT_PUBLIC_API_PASSWORD || ''
+
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseURL: base_url,
+    auth: {
+        username: username,
+        password: password
+    },
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json"
