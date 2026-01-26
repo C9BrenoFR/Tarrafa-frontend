00944cc53b4c34edd2aad424d2cdb2131a0cf8d0
Yan-Fonseca <yanfonseca2002@gmail.com>
2026-01-26 14:19:18 -0300

    [FIX] tooltip

--- src/components/template/tooltip.tsx
+++ src/components/template/tooltip.tsx
@@ -98,7 +98,7 @@ export function Tooltip({ message }: TooltipIndicatorProps) {
                   onClick={closeModal}
                   className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:cursor-pointer hover:bg-blue-600 transition-colors duration-200 font-medium"
                 >
-                  Entendi
+                  Ok
                 </button>
               </div>
             </div>
