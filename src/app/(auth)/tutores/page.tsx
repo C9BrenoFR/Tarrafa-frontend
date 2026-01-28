750ea203aa45d3db7f499d27f508eb8a16dc540a
C9BrenoFR <brenufr@gmail.com>
2026-01-28 09:56:32 -0300

    feat: implement tutors page layout with GeneralData, Indicators, and HomeRanking components

--- src/app/(auth)/tutores/page.tsx
+++ src/app/(auth)/tutores/page.tsx
@@ -1,8 +1,22 @@
+'use client';
+
+import GeneralData from "@/components/pages/tutores/general-data";
+import Indicators from "@/components/pages/tutores/Indicator/indicators";
+import HomeRanking from "@/components/pages/tutores/ranking";
+import PageTemplate from "@/components/template/page-template";
 
 export default function Page() {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <PageTemplate
+            title="Visão Geral"
+            subTitle="Da Instituição"
+        >
+            <GeneralData />
+            <Indicators />
+            <div className="flex flex-row space-x-3">
+                <HomeRanking type='best-performance' />
+                <HomeRanking type='at-risk' />
+            </div>
+        </PageTemplate>
     );
 };
